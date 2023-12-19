490f56fb850ea537b93c4b262a2c261981404c87
dilrajs616 <dilraj082@gmail.com>
2023-12-19 11:20:56 +0530

    Made Search Page

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask,render_template as rt,session, redirect, url_for, reques
 
 app = Flask(__name__)
 
+app.secret_key = 'BCQWR#$@@WE@12332423@121'
+
 @app.route("/")
 def landing():
+    return rt("index.html")
+
+@app.route("/categories", methods=["POST"])
+def category():
+    query = request.form.get("searchquery")
+    print(query)
     return rt("index.html")
 
 if __name__ == '__main__':
